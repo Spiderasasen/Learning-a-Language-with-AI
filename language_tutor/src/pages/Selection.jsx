c64f5b2faad34ce55be1226e196310c7c1b64d49
spiderasasen <spiderasasen@gmail.com>
2026-09-07 10:16:47 -0400

    got the style for the button right now i need to find a better image for drilling and adding a bit more stuff there

--- language_tutor/src/pages/Selection.jsx
+++ language_tutor/src/pages/Selection.jsx
@@ -2,6 +2,7 @@ import "../styles/main.css"
 import Drilling from "../assets/477-4770692_transparent-study-icon-png-studying-clipart-black-and.png"
 import Practice from "../assets/two-people-conversing-with-language-symbols-solid-icon-multilingual-communication-translation-and-language-exchange-silhouette-symbol-isolated-illustration-vector.jpg"
 import "../styles/selection.css"
+import {NavLink} from "react-router-dom";
 
 function Selection() {
     return (
@@ -9,12 +10,14 @@ function Selection() {
             <div className="selection_card">
                 <h1>Selection</h1>
 
-
                 {/*selection on 2 systems, drilling or practice*/}
                 <div id="image_section">
-
                     <img src={Practice} alt="Practice" className="image"/>
                     <img src={Drilling} alt="Drilling" className="image"/>
+                </div>
+
+                <div className="button_section">
+                    <NavLink to="/" className="button">Go Back</NavLink>
                 </div>
             </div>
         </div>
