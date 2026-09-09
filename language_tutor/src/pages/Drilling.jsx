389b51858baeaca6c46120184c13ce66700eef0d
spiderasasen <spiderasasen@gmail.com>
2026-09-09 10:40:47 -0400

    finished the style section. now to work on adding the level and the language from the selection tab

--- language_tutor/src/pages/Drilling.jsx
+++ language_tutor/src/pages/Drilling.jsx
@@ -1,6 +1,36 @@
+import "../styles/main.css"
+import "../styles/drilling.css"
+import {NavLink} from "react-router-dom";
+
 function Drilling() {
     return (
-        <div>Drilling</div>
+        <div className="main">
+            <div id="drilling_card">
+                <div className="card-title">
+                    <h1>Drilling</h1>
+                    <h2>Insert Language here: insert level here</h2>
+                </div>
+                <div id="drilling_content">
+                    <h2>Word</h2>
+                    <p>word defination in english goes here</p>
+                </div>
+                <div id="user_input">
+                    <input type="text" placeholder="Type a sentince here"/>
+                </div>
+                <div className="card-footer">
+                    <button>Send</button>
+                </div>
+            </div>
+            <div id="ai_summary">
+                <div className="summary">
+                    <h2>Summary</h2>
+                    <p>ai summary goes here</p>
+                </div>
+                <div className="button_sectiont">
+                    <NavLink to="/selection" className="button">Go Back</NavLink>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Drilling;
